9f3fa525445c6ef9fa18c59b1e6649030668acfb
SameerShetty <kalyanshettisameer@gmail.com>
2023-05-31 20:46:01 +0530

    runs fine locally with thunderClient

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.status(200).json("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  const { first, second } = req.body;
+  if (!first || !second) {
+    return res.status(400).json({ message: "Invalid request !!!" });
+  }
+  return res.status(200).json({ result: Number(first) + Number(second) });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  const { first, second } = req.body;
+  if (!first || !second) {
+    return res.status(400).json({ message: "Invalid request !!!" });
+  }
+  return res.status(200).json({ result: Number(first) - Number(second) });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
